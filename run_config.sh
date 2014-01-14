86a6eebe98f459bbdf1f7b28454937cef61ed497
felipe <fjuarezmurillo@synergyj.com>
2014-01-14 12:43:08 -0600

    agregando archivo de configuración

---
+++ run_config.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# echo "\033[0;34mCloning .vim from fjuarezmurillo repository...\033[0m"
+# hash git >/dev/null && /usr/bin/env git clone https://github.com/fjuarezmurillo/.vim.git ~/.vim || {
+#   echo "git not installed"
+#   exit
+# }
+
+echo "\033[0;34mCreating bundle directory...\033[0m"
+mkdir -p ~/.vim/bundle
+
+echo "\033[0;34mCreating tmp directory for saving swp files...\033[0m"
+mkdir -p ~/.vim/tmp
+
+echo "\033[0;34mCloning neobundle repository...\033[0m"
+/usr/bin/env git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
