abbeb44da2107fd978f015c5554c97cd2f5247d1
felipe <fjuarezmurillo@synergyj.com>
2014-01-16 18:22:30 -0600

    change url repository and creating symbolic link at the end

--- run_config.sh
+++ run_config.sh
@@ -1,5 +1,5 @@
-echo "\033[0;34mCloning .vim from fjuarezmurillo repository...\033[0m"
-hash git >/dev/null && /usr/bin/env git clone https://github.com/fjuarezmurillo/.vim.git ~/.vim || {
+echo "\033[0;34mCloning .vim from sohjiro repository...\033[0m"
+hash git >/dev/null && /usr/bin/env git clone https://github.com/sohjiro/.vim.git ~/.vim || {
   echo "git not installed"
   exit
 }
@@ -12,3 +12,5 @@ mkdir -p ~/.vim/tmp
 
 echo "\033[0;34mCloning neobundle repository...\033[0m"
 /usr/bin/env git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+ln -s ~/.vim/vimrc ~/.vimrc
