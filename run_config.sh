a889b3a67431b35bffeb04dfff91e3e5a4ba2db2
sohjiro <felipe@makingdevs.com>
2017-09-10 15:19:33 -0500

    testing new configuration

--- run_config.sh
+++ run_config.sh
@@ -36,14 +36,26 @@ hash git >/dev/null && /usr/bin/env git clone git@github.com:sohjiro/.vim.git ~/
   exit
 }
 
-echo "\033[0;34mCreating bundle directory...\033[0m"
-mkdir -p ~/.vim/bundle
-
 echo "\033[0;34mCreating tmp directory for saving swp files...\033[0m"
 mkdir -p ~/.vim/tmp
 
-echo "\033[0;34mCloning neobundle repository...\033[0m"
-/usr/bin/env git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+echo "\033[0;34mCreating autoload file...\033[0m"
+mkdir -p ~/.vim/autoload
+
+echo "\033[0;34mCloning vim-pluging to autoload...\033[0m"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# /usr/bin/env git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+# create dir custom
+# create file plugged inside custom
+# create file config inside custom
+
+# echo "\033[0;34mCreating bundle directory...\033[0m"
+# mkdir -p ~/.vim/bundle
+
+# echo "\033[0;34mCloning neobundle repository...\033[0m"
+# /usr/bin/env git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 echo "\033[0;34mCreating symbolic link...\033[0m"
 ln -s ~/.vim/vimrc ~/.vimrc
